10a72ddfa84a5751ea15eff0bde99141f03969a4
vir-thi-w11 <albert.tan.2.pi.0@gmail.com>
2024-01-04 02:55:19 -0300

    🚀 RELEASE: first commit to add project files

---
+++ PhoneAppFastBlazorMaui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PhoneAppFastBlazorMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
