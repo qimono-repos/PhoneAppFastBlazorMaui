641779737707d99300533dab9bca77b32f9dafe0
vir-thi-w11 <albert.tan.2.pi.0@gmail.com>
2024-01-08 17:09:23 -0300

    👌 IMPROVE: Implement builder patter to use a dynamic logic implementation

--- ComponentAppFastRazorClassLibrary/FastDeffinitionComponent.razor.cs
+++ ComponentAppFastRazorClassLibrary/FastDeffinitionComponent.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel.Design.Serialization;
+using System.Text;
 
 namespace ComponentAppFastRazorClassLibrary
 {
@@ -8,18 +9,14 @@ namespace ComponentAppFastRazorClassLibrary
         StringBuilder sb = new StringBuilder();
         string htmlSnippet = string.Empty;
         string htmlListSnippet = string.Empty;
+        string htmlDynamicListSnippet = string.Empty;
 
         string[]? words;
+        SnippetHtmlBuilder snippetHtmlBuilder = new SnippetHtmlBuilder("ul");
 
         protected override void OnInitialized()
         {
-            sb.Append("<h1>");
-            sb.Append(hello);
-            sb.Append("</h1>");
-            htmlSnippet = sb.ToString();
-
-            words = new[] { "word  1", "word 2" };
-
+            words = new[] { "AAAAAA", "BBBBBB" };
             sb.Clear();
             sb.Append("<ul>");
 
@@ -29,6 +26,74 @@ namespace ComponentAppFastRazorClassLibrary
             }
             sb.Append("</ul>");
             htmlListSnippet = sb.ToString();
+
+            //snippetHtmlBuilder = new SnippetHtmlBuilder("ul");
+            snippetHtmlBuilder.AddChild("li","Forget");
+            snippetHtmlBuilder.AddChild("li","Active");
+            snippetHtmlBuilder.AddChild("li","State");
+            snippetHtmlBuilder.AddChild("li","Teach");
+
+            string snippet = snippetHtmlBuilder.ToString()?? "No text to show";
+            htmlDynamicListSnippet = snippet;
+        }
+    }
+    public class SnippetHtmlElement
+    {
+        public string? Name, Text;
+        public List<SnippetHtmlElement> HtmlElements = new List<SnippetHtmlElement>();
+        private const int _indentSize = 2;
+        public SnippetHtmlElement() { }
+        public SnippetHtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Text = text ?? throw new ArgumentNullException(paramName: nameof(name));
+        }
+
+        private string ToStringImplementation(int indentation)
+        {
+            StringBuilder sb = new StringBuilder();
+            int indentationFormula = _indentSize * indentation;
+            int indentationTextFormula = _indentSize * (indentationFormula + 1);
+            string indentSpaces = new string(' ', indentationFormula);
+            sb.Append($"{indentSpaces}<{Name}>");
+            //if (string.IsNullOrWhiteSpace(Text)) return sb.ToString();
+            if (!string.IsNullOrEmpty(Text))
+            {
+                sb.Append(new string(' ', indentationTextFormula));
+                sb.AppendLine(Text);
+            }
+            foreach (var element in HtmlElements)
+                sb.Append(element.ToStringImplementation(indentation + 1));
+            sb.AppendLine($"{indentSpaces}<{Name}/>");
+
+            return sb.ToString();
+        }
+        public override string ToString()
+        {
+            return ToStringImplementation(0);
+        }
+    }
+    public class SnippetHtmlBuilder
+    {
+        private readonly string _rootName;
+        SnippetHtmlElement _root = new SnippetHtmlElement();
+        public SnippetHtmlBuilder(string rootName)
+        {
+            _rootName = rootName;
+            _root.Name = rootName;
+        }
+        public void AddChild(string childName, string childText)
+        {
+            SnippetHtmlElement element = new SnippetHtmlElement(childName, childText);
+            _root.HtmlElements.Add(element);
+        }
+        public override string ToString()
+        {
+            return _root.ToString();
+        }
+        public void Clear()
+        {
+            _root = new SnippetHtmlElement();
         }
     }
 }
