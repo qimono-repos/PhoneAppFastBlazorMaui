d2e98a9395ef0c6dd5c6dfab5d9a325540a4c108
vir-thi-w11 <albert.tan.2.pi.0@gmail.com>
2024-01-08 13:38:19 -0300

    📦 NEW: bind element to show html snippet

---
+++ ComponentAppFastRazorClassLibrary/FastDeffinitionComponent.razor.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace ComponentAppFastRazorClassLibrary
+{
+    public partial class FastDeffinitionComponent
+    {
+        string hello = "The HTML snippet ";
+        StringBuilder sb = new StringBuilder();
+        string htmlSnippet = string.Empty;
+        string htmlListSnippet = string.Empty;
+
+        string[]? words;
+
+        protected override void OnInitialized()
+        {
+            sb.Append("<h1>");
+            sb.Append(hello);
+            sb.Append("</h1>");
+            htmlSnippet = sb.ToString();
+
+            words = new[] { "word  1", "word 2" };
+
+            sb.Clear();
+            sb.Append("<ul>");
+
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}<li>", word);
+            }
+            sb.Append("</ul>");
+            htmlListSnippet = sb.ToString();
+        }
+    }
+}
